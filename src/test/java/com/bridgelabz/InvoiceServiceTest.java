d8ee969f85b3d38c82480db2b403593d557b7c82
dhirajrathod <dhirajrathod3000@gmail.com>
2021-08-20 12:17:20 +0530

    ADDED: Calculate Multiple Fare

--- src/test/java/com/bridgelabz/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -1,7 +1,6 @@
 package com.bridgelabz;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class InvoiceServiceTest {
@@ -9,28 +8,49 @@ public class InvoiceServiceTest {
 
     /**
      * Given Distance and Time
-     * return total fare
+     * return totalfare
      */
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
+
         invoiceGenerator = new InvoiceGenerator();
         double distance = 2.0;
         int time = 5;
+
         double fare = invoiceGenerator.calculateFare(distance, time);
+
         Assert.assertEquals(25, fare, 0.0);
     }
 
     /**
      * 
      * Given Less Distance and Time
-     * return minimum fare as 5
+     * return minimum fare 5
      */
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+
         invoiceGenerator = new InvoiceGenerator();
         double distance = 0.1;
         int time = 1;
+
         double fare = invoiceGenerator.calculateFare(distance, time);
+
         Assert.assertEquals(5, fare, 0.0);
+    }
+
+    /**
+     * Given Multiple Rides Should Return Total Fare
+     */
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+
+        invoiceGenerator = new InvoiceGenerator();
+        Ride[] rides = {new Ride(2.0, 5),
+                new Ride(0.1, 1)};
+
+        double fare = invoiceGenerator.calculateTotalFare(rides);
+        
+        Assert.assertEquals(30, fare, 0.0);
     }
 }
